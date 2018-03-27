f57e348d9bf2d79176e02add1f720973e6a22f3a
colining <18810782954@163.com>
2018-03-27 10:29:01 +0800

    complete section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,7 +6,13 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        for (List<String> stringList : object.values()) {
+            for (String s : stringList) {
+                if (collectionA.containsKey(s)) {
+                    collectionA.put(s, collectionA.get(s) - 1);
+                }
+            }
+        }
+        return collectionA;
     }
 }
--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,37 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> map = countSameElements(collectionA);
+        return getStringIntegerMap(object, map);
+    }
 
-        return null;
+    private Map<String, Integer> getStringIntegerMap(Map<String, List<String>> object, Map<String, Integer> map) {
+        for (List<String> stringList : object.values()) {
+            for (String s : stringList) {
+                if (map.containsKey(s)) {
+                    map.put(s, map.get(s) - map.get(s)/3);
+                }
+            }
+        }
+        return map;
+    }
+
+    Map<String,Integer> countSameElements(List<String> collection1) {
+        //实现练习要求，并改写该行代码。
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (String s : collection1) {
+            if (hashMap.containsKey(s)) {
+                hashMap.put(s, hashMap.get(s) + 1);
+            }else {
+                hashMap.put(s, 1);
+            }
+        }
+        return hashMap;
     }
 }
